904491de457be88cbfeea278dd4cbf30cf22f8cb
chenwei <2282049751@qq.com>
2025-08-12 14:57:43 +0800

    refactor(botadmin-spotbot): 优化机器人关闭和删除逻辑
    - 修改机器人关闭逻辑，增加根据返回状态码判断机器人状态的处理
    - 更新机器人删除逻辑，增加判断机器人是否已关闭的处理
    - 优化错误提示信息，使其更加准确地反映机器人状态

--- src/views/botadmin/spotbot/index.vue
+++ src/views/botadmin/spotbot/index.vue
@@ -113,24 +113,23 @@ const handleEdit = (record) => {
 const closeRobot = async (record) => {
   const params = { ids: record.id, active: 5 }
   const activeResp = await api.getActive(params)
-  console.log(activeResp)
   if (activeResp.code === 200) {
-    if (activeResp.data == 2 || activeResp.data == 1) {
+    if (activeResp.data.active == 2 || activeResp.data.active == 1) {
       Message.error('请先暂停机器人才能关闭！')
       return
-    } else if (activeResp.data == 5) {
-      Message.warning('机器人已关闭！')
+    } else if (activeResp.data.active == 5) {
+      Message.warning('机器人关闭中！')
       crudRef.value?.refresh()
       return
-    } else if (activeResp.data == 6) {
-      Message.warning('机器人已平仓！')
+    } else if (activeResp.data.active == 6) {
+      Message.warning('机器人已关闭！')
       crudRef.value?.refresh()
       return
     }
   }
   const closeResp = await api.setActive(params)
   if (closeResp.code === 200) {
-    Message.success(`机器人关闭成功！`)
+    Message.success(`机器人即将关闭！`)
     crudRef.value?.refresh()
   }
 }
@@ -166,8 +165,12 @@ const options = reactive({
     func: async (params) => {
       const closeResp = await api.getActive(params)
       if (closeResp.code === 200) {
-        if (closeResp.data != 6) {
-          Message.error('请先关闭机器人,机器人平仓后，才能删除！')
+        if (closeResp.data.status != 5) {
+          Message.error('请先关闭机器人,才能删除！')
+          return
+        }
+        if (closeResp.data.active != 6) {
+          Message.error('机器人关闭中，请稍后删除！')
           return
         }
       }
