8ebb982c670114b74358c8538e8913fd1b4cfedf
chenwei <2282049751@qq.com>
2025-08-18 10:29:39 +0800

    refactor(ma-buttonMenu): 优化移动端菜单
    - 将机器人菜单归类到量化菜单内
    - 更新子菜单数据源为 mobileRouters 并添加量化管理项

--- src/layout/components/ma-buttonMenu.vue
+++ src/layout/components/ma-buttonMenu.vue
@@ -12,7 +12,7 @@
       </div>
       <template #content>
         <a-menu mode="popButton" showCollapseButton :popup-max-height="360">
-          <children-menu v-model="userStore.routers" />
+          <children-menu v-model="mobileRouters" />
         </a-menu>
       </template>
     </a-trigger>
@@ -26,6 +26,64 @@ import { useAppStore, useUserStore } from '@/store'
 import ChildrenMenu from './components/children-menu.vue'
 
 const userStore = useUserStore()
+let mobileRouters = userStore.routers
+
+let exchangeTtem = mobileRouters.find((item) => item.name === 'exchange')
+let accountItem = mobileRouters.find((item) => item.name === 'account')
+let symbolItem = mobileRouters.find((item) => item.name === 'symbol')
+let spotBotItem = mobileRouters.find((item) => item.name === 'spotBot')
+let coinBotItem = mobileRouters.find((item) => item.name === 'coinBot')
+let spotBotTradeItem = mobileRouters.find((item) => item.name === 'spotBotTrade')
+let coinBotTradeItem = mobileRouters.find((item) => item.name === 'coinBotTrade')
+let spotBotLogItem = mobileRouters.find((item) => item.name === 'spotBotLog')
+let coinBotLogItem = mobileRouters.find((item) => item.name === 'coinBotLog')
+let botItem = {
+  name: 'bot',
+  meta: {
+    title: '量化管理',
+    icon: 'icon-robot',
+  },
+  path: '/bot',
+  children: [],
+}
+if (exchangeTtem) {
+  botItem.children.push(exchangeTtem)
+  mobileRouters = mobileRouters.filter((r) => r !== exchangeTtem) // 移除 exchangeTtem
+}
+if (accountItem) {
+  botItem.children.push(accountItem)
+  mobileRouters = mobileRouters.filter((r) => r !== accountItem) // 移除 accountItem
+}
+if (symbolItem) {
+  botItem.children.push(symbolItem)
+  mobileRouters = mobileRouters.filter((r) => r !== symbolItem) // 移除 symbolItem
+}
+if (spotBotItem) {
+  botItem.children.push(spotBotItem)
+  mobileRouters = mobileRouters.filter((r) => r !== spotBotItem) // 移除 spotBotItem
+}
+if (coinBotItem) {
+  botItem.children.push(coinBotItem)
+  mobileRouters = mobileRouters.filter((r) => r !== coinBotItem) // 移除 coinBotItem
+}
+if (spotBotTradeItem) {
+  botItem.children.push(spotBotTradeItem)
+  mobileRouters = mobileRouters.filter((r) => r !== spotBotTradeItem) // 移除 spotBotTradeItem
+}
+if (coinBotTradeItem) {
+  botItem.children.push(coinBotTradeItem)
+  mobileRouters = mobileRouters.filter((r) => r !== coinBotTradeItem) // 移除 coinBotTradeItem
+}
+if (spotBotLogItem) {
+  botItem.children.push(spotBotLogItem)
+  mobileRouters = mobileRouters.filter((r) => r !== spotBotLogItem) // 移除 spotBotLogItem
+}
+if (coinBotLogItem) {
+  botItem.children.push(coinBotLogItem)
+  mobileRouters = mobileRouters.filter((r) => r !== coinBotLogItem) // 移除 coinBotLogItem
+}
+mobileRouters.push(botItem)
+
 const popupVisible = ref(false)
 
 </script>
