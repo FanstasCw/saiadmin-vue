0dba66d61b9613d7fa484bdde8af0f407b0fc464
chenwei <2282049751@qq.com>
2025-08-18 11:30:11 +0800

    refactor(dashboard): 重构仪表盘组件
    - 移除了 st-count 组件，增加了 trade-count 组件
    - 移除了 st-login-chart 组件，增加了 trade-table 组件
    - 注释掉了 st-announced 组件
    - 删除了未使用的 StSaiadmin 导入

--- src/views/dashboard/components/statistics.vue
+++ src/views/dashboard/components/statistics.vue
@@ -1,16 +1,17 @@
 <template>
   <st-welcome />
-  <st-count />
-  <st-login-chart />
+  <trade-count />
+  <trade-table />
 
   <div class="block lg:flex">
-    <st-announced />
+    <!-- <st-announced /> -->
     <!-- <st-saiadmin /> -->
   </div>
 </template>
 
 <script setup>
-import StCount from './components/st-count.vue'
+import TradeCount from './components/trade-count.vue'
+import TradeTable from './components/trade-table.vue'
 import StWelcome from './components/st-welcome.vue'
 import StLoginChart from './components/st-loginChart.vue'
 // import StSaiadmin from './components/st-saiadmin.vue'
