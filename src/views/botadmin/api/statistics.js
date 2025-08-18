0511887ea919de94591761234a8be9e42472ea85
chenwei <2282049751@qq.com>
2025-08-18 11:03:09 +0800

    feat(botadmin): 添加币本位套利机器人 API 接口
    - 新增 getBotCount 方法，用于获取正在运行机器人的数量
    - 新增 getBotData 方法，用于获取机器人数据

---
+++ src/views/botadmin/api/statistics.js
@@ -0,0 +1,27 @@
+import { request } from '@/utils/request.js'
+
+/**
+ * 币本位套利机器人 API接口
+ */
+export default {
+  /**
+   * 获取正在运行机器人的数量
+   * @returns
+   */
+  getBotCount() {
+    return request({
+      url: '/bot/statistics/botCount',
+      method: 'get'
+    })
+  },
+  /**
+   * 获取机器人数据
+   * @returns
+   */
+  getBotData() {
+    return request({
+      url: '/bot/statistics/botData',
+      method: 'get'
+    })
+  }
+}
