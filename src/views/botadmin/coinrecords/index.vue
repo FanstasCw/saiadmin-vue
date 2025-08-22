1a7a5b6bc76ca9d12451c858941c5a42e3de89ee
chenwei <2282049751@qq.com>
2025-08-22 17:27:09 +0800

    feat(botadmin): 优化现货和合约记录页面
    - 添加国际化支持，使用 i18n 对文本进行翻译
    - 为搜索栏和表格列添加更多合适的数据
    - 优化搜索栏和表格列的显示，使其更加合理
    - 为两个页面添加交易对数据

--- src/views/botadmin/coinrecords/index.vue
+++ src/views/botadmin/coinrecords/index.vue
@@ -4,32 +4,40 @@
       <!-- 搜索区 tableSearch -->
       <template #tableSearch>
         <a-col :sm="8" :xs="24">
-          <a-form-item label="机器人" field="coin_bot_id">
-            <a-select v-model="searchForm.coin_bot_id" :options="botName" placeholder="请选择机器人" allow-clear />
+          <a-form-item :label="t('bot.botName')" field="coin_bot_id">
+            <a-select
+              v-model="searchForm.coin_bot_id"
+              :options="botName"
+              :placeholder="t('bot.selectBot')"
+              allow-clear />
           </a-form-item>
         </a-col>
         <a-col :sm="8" :xs="24">
-          <a-form-item label="订单ID" field="exchange_order_id">
-            <a-input v-model="searchForm.exchange_order_id" placeholder="请输入订单ID" allow-clear />
+          <a-form-item :label="t('bot.orderId')" field="exchange_order_id">
+            <a-input v-model="searchForm.exchange_order_id" :placeholder="t('bot.inputOrderId')" allow-clear />
           </a-form-item>
         </a-col>
         <a-col :sm="8" :xs="24">
-          <a-form-item label="交易对" field="symbol">
-            <a-select v-model="searchForm.symbol" :options="[]" placeholder="请选择交易对" allow-clear />
+          <a-form-item :label="t('bot.symbol')" field="symbol">
+            <a-select
+              v-model="searchForm.symbol"
+              :options="symbolData"
+              :placeholder="t('bot.selectSymbol')"
+              allow-clear />
           </a-form-item>
         </a-col>
         <a-col :sm="8" :xs="24">
-          <a-form-item label="买卖方向" field="side">
-            <sa-select v-model="searchForm.side" dict="coin_side" placeholder="请选择买卖方向" allow-clear />
+          <a-form-item :label="t('bot.side')" field="side">
+            <sa-select v-model="searchForm.side" dict="coin_side" :placeholder="t('bot.selectSide')" allow-clear />
           </a-form-item>
         </a-col>
         <a-col :sm="8" :xs="24">
-          <a-form-item label="订单类型" field="type">
-            <sa-select v-model="searchForm.type" dict="order_type" placeholder="请选择订单类型" allow-clear />
+          <a-form-item :label="t('bot.type')" field="type">
+            <sa-select v-model="searchForm.type" dict="order_type" :placeholder="t('bot.selectType')" allow-clear />
           </a-form-item>
         </a-col>
         <a-col :sm="8" :xs="24">
-          <a-form-item label="成交时间" field="create_time">
+          <a-form-item :label="t('bot.createTime')" field="create_time">
             <a-range-picker v-model="searchForm.create_time" :show-time="true" mode="date" />
           </a-form-item>
         </a-col>
@@ -59,13 +67,16 @@
 import { onMounted, ref, reactive } from 'vue'
 import api from '../api/coinrecords'
 import commonApi from '@/api/common'
+import { useI18n } from 'vue-i18n'
 
 // 引用定义
 const crudRef = ref()
 const editRef = ref()
 const viewRef = ref()
 const botName = ref([])
+const symbolData = ref([])
 
+const { t } = useI18n()
 // 搜索表单
 const searchForm = ref({
   coin_bot_id: '',
@@ -85,25 +96,29 @@ const options = reactive({
   showTools: true,
   showSort: false,
   operationColumn: false,
+  searchText: t('bot.search'),
+  resetText: t('bot.reset'),
 })
 
 // SaTable 列配置
 const columns = reactive([
-  { title: '机器人', dataIndex: 'coin_bot_id', sortable: { sortDirections: ['ascend', 'descend'] } },
-  { title: '订单ID', dataIndex: 'exchange_order_id' },
-  { title: '交易对', dataIndex: 'symbol', sortable: { sortDirections: ['ascend', 'descend'] } },
-  { title: '买卖方向', dataIndex: 'side', type: 'dict', dict: 'coin_side' },
-  { title: '订单类型', dataIndex: 'type', type: 'dict', dict: 'order_type' },
-  { title: '订单数量(张)', dataIndex: 'amount' },
-  { title: '订单价格', dataIndex: 'price' },
-  { title: '手续费', dataIndex: 'fee' },
-  { title: '成交时间', dataIndex: 'create_time', sortable: { sortDirections: ['ascend', 'descend'] } },
+  { title: t('bot.botName'), dataIndex: 'coin_bot_id', sortable: { sortDirections: ['ascend', 'descend'] } },
+  { title: t('bot.orderId'), dataIndex: 'exchange_order_id' },
+  { title: t('bot.symbol'), dataIndex: 'symbol', sortable: { sortDirections: ['ascend', 'descend'] } },
+  { title: t('bot.side'), dataIndex: 'side', type: 'dict', dict: 'coin_side' },
+  { title: t('bot.type'), dataIndex: 'type', type: 'dict', dict: 'order_type' },
+  { title: t('bot.amount'), dataIndex: 'amount' },
+  { title: t('bot.price'), dataIndex: 'price' },
+  { title: t('bot.fee'), dataIndex: 'fee' },
+  { title: t('bot.createTime'), dataIndex: 'create_time', sortable: { sortDirections: ['ascend', 'descend'] } },
 ])
 
 // 页面数据初始化
 const initPage = async () => {
   const postResp = await commonApi.commonGet('/bot/coinBot/getBotName')
   botName.value = postResp.data
+  const symbolResp = await commonApi.commonGet('/bot/symbol/accessSymbol?type=2')
+  symbolData.value = symbolResp.data
 }
 
 // SaTable 数据请求
--- src/views/botadmin/spotrecords/index.vue
+++ src/views/botadmin/spotrecords/index.vue
@@ -9,32 +9,40 @@
       <!-- 搜索区 tableSearch -->
       <template #tableSearch>
         <a-col :sm="8" :xs="24">
-          <a-form-item label="机器人" field="spot_bot_id">
-            <a-select v-model="searchForm.spot_bot_id" :options="botName" placeholder="请选择机器人" allow-clear />
+          <a-form-item :label="t('bot.botName')" field="spot_bot_id">
+            <a-select
+              v-model="searchForm.spot_bot_id"
+              :options="botName"
+              :placeholder="t('bot.selectBot')"
+              allow-clear />
           </a-form-item>
         </a-col>
         <a-col :sm="8" :xs="24">
-          <a-form-item label="订单ID" field="exchange_order_id">
-            <a-input v-model="searchForm.exchange_order_id" placeholder="请输入订单ID" allow-clear />
+          <a-form-item :label="t('bot.orderId')" field="exchange_order_id">
+            <a-input v-model="searchForm.exchange_order_id" :placeholder="t('bot.inputOrderId')" allow-clear />
           </a-form-item>
         </a-col>
         <a-col :sm="8" :xs="24">
-          <a-form-item label="交易对" field="symbol">
-            <a-select v-model="searchForm.symbol" :options="[]" placeholder="请选择交易对" allow-clear />
+          <a-form-item :label="t('bot.symbol')" field="symbol">
+            <a-select
+              v-model="searchForm.symbol"
+              :options="symbolData"
+              :placeholder="t('bot.selectSymbol')"
+              allow-clear />
           </a-form-item>
         </a-col>
         <a-col :sm="8" :xs="24">
-          <a-form-item label="买卖方向" field="side">
-            <sa-select v-model="searchForm.side" dict="spot_side" placeholder="请选择买卖方向" allow-clear />
+          <a-form-item :label="t('bot.side')" field="side">
+            <sa-select v-model="searchForm.side" dict="spot_side" :placeholder="t('bot.selectSide')" allow-clear />
           </a-form-item>
         </a-col>
         <a-col :sm="8" :xs="24">
-          <a-form-item label="订单类型" field="type">
-            <sa-select v-model="searchForm.type" dict="order_type" placeholder="请选择订单类型" allow-clear />
+          <a-form-item :label="t('bot.type')" field="type">
+            <sa-select v-model="searchForm.type" dict="order_type" :placeholder="t('bot.selectType')" allow-clear />
           </a-form-item>
         </a-col>
         <a-col :sm="8" :xs="24">
-          <a-form-item label="成交时间" field="create_time">
+          <a-form-item :label="t('bot.createTime')" field="create_time">
             <a-range-picker v-model="searchForm.create_time" :show-time="true" mode="date" />
           </a-form-item>
         </a-col>
@@ -64,12 +72,16 @@
 import { onMounted, ref, reactive } from 'vue'
 import api from '../api/spotrecords'
 import commonApi from '@/api/common'
+import { useI18n } from 'vue-i18n'
 
 // 引用定义
 const crudRef = ref()
 const editRef = ref()
 const viewRef = ref()
 const botName = ref([])
+const symbolData = ref([])
+
+const { t } = useI18n()
 
 // 搜索表单
 const searchForm = ref({
@@ -90,26 +102,40 @@ const options = reactive({
   showTools: true,
   showSort: false,
   operationColumn: false,
+  searchText: t('bot.search'),
+  resetText: t('bot.reset'),
 })
 
 // SaTable 列配置
 const columns = reactive([
-  { title: '机器人', dataIndex: 'spot_bot_id', sortable: { sortDirections: ['ascend', 'descend'] }, width: 160 },
-  { title: '订单ID', dataIndex: 'exchange_order_id', width: 160 },
-  { title: '交易对', dataIndex: 'symbol', sortable: { sortDirections: ['ascend', 'descend'] }, width: 120 },
-  { title: '买卖方向', dataIndex: 'side', type: 'dict', dict: 'spot_side', width: 120 },
-  { title: '订单类型', dataIndex: 'type', type: 'dict', dict: 'order_type', width: 120 },
-  { title: '订单数量', dataIndex: 'amount', width: 180 },
-  { title: '订单价格', dataIndex: 'price', width: 180 },
-  { title: '成交金额', dataIndex: 'cost', width: 180 },
-  //{ title: '手续费', dataIndex: 'fee' },
-  { title: '成交时间', dataIndex: 'create_time', sortable: { sortDirections: ['ascend', 'descend'] }, width: 180 },
+  {
+    title: t('bot.botName'),
+    dataIndex: 'spot_bot_id',
+    sortable: { sortDirections: ['ascend', 'descend'] },
+    width: 160,
+  },
+  { title: t('bot.orderId'), dataIndex: 'exchange_order_id', width: 160 },
+  { title: t('bot.symbol'), dataIndex: 'symbol', sortable: { sortDirections: ['ascend', 'descend'] }, width: 120 },
+  { title: t('bot.side'), dataIndex: 'side', type: 'dict', dict: 'spot_side', width: 120 },
+  { title: t('bot.type'), dataIndex: 'type', type: 'dict', dict: 'order_type', width: 120 },
+  { title: t('bot.amount'), dataIndex: 'amount', width: 180 },
+  { title: t('bot.price'), dataIndex: 'price', width: 180 },
+  { title: t('bot.cost'), dataIndex: 'cost', width: 180 },
+  { title: t('bot.fee'), dataIndex: 'fee', width: 180 },
+  {
+    title: t('bot.createTime'),
+    dataIndex: 'create_time',
+    sortable: { sortDirections: ['ascend', 'descend'] },
+    width: 180,
+  },
 ])
 
 // 页面数据初始化
 const initPage = async () => {
   const postResp = await commonApi.commonGet('/bot/spotBot/getBotName')
   botName.value = postResp.data
+  const symbolResp = await commonApi.commonGet('/bot/symbol/accessSymbol?type=1')
+  symbolData.value = symbolResp.data
 }
 
 // SaTable 数据请求
