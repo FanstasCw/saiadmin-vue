6732698bee45c0de3d4263c30152d976848a56c9
chenwei <2282049751@qq.com>
2025-09-02 09:51:01 +0800

    feat(layout): 添加底部栏组件
    - 新增 ma-footer.vue 文件
    - 实现了一个简单的底部栏，显示公司名称
    - 调整了样式，包括高度、颜色和对齐方式

---
+++ src/layout/components/ma-footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <a-layout-footer class="footer">MARSQUANT AI TECHNOLOGY LIMITED</a-layout-footer>
+</template>
+
+<script setup></script>
+
+<style lang="less" scoped>
+.footer {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 30px;
+  color: var(--color-text-2);
+  background-color: #f2f3f5;
+  text-align: center;
+}
+</style>
